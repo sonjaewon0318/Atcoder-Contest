7b898c153bf42de9744453a312256ab26a86001d
Jae Won Son <63086633+sonjaewon0318@users.noreply.github.com>
2020-12-21 12:43:56 +0900

    Create A. Brick.cpp

---
+++ anasonic Programming Contest (AtCoder Beginner Contest 186)/A. Brick.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int a, b; cin >> a >> b;
+    cout << a / b;
+}
